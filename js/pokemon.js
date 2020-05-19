a271e55717481e1e85f8c66699a735c0d980effb
christianzolin <christian.koffer@gmail.com>
2020-05-19 03:30:39 -0300

    criando carrossel dinamico, gerando primeiro pokemon aleatóriamente.

--- js/pokemon.js
+++ js/pokemon.js
@@ -6,36 +6,54 @@ document.addEventListener('DOMContentLoaded', function () {
     axios.get('https://pokeapi.co/api/v2/pokemon?offset=001&limit=251')
         .then(resultado => {
             pokemons = resultado.data.results
+            var primeiroPoke = pokemons[Math.floor(Math.random()*pokemons.length)];
             pokemons.forEach(poke => {
                 var split = poke.url.split('/')
                 var numero = split[split.length - 2]
                 var url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + numero + '.png'
                 poke_imagens[poke.name] = url
             })
+            buscarPokemon(primeiroPoke.name)
         })
     var elems = document.querySelectorAll('.autocomplete');
     var instances = M.Autocomplete.init(elems, { data: poke_imagens });
 
 });
 
-function buscarPokemon() {
+function onPress(){
     var input = document.querySelector('#autocomplete-input');
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${input.value}`)
-        .then(resultado =>{
+    buscarPokemon(input.value)
+}
+
+function buscarPokemon(nome) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${nome}`)
+        .then(resultado => {
             document.querySelector('#nomePokemon').innerHTML = resultado.data.name.toUpperCase()
-            var lista =  document.querySelector('#listaHabilidades');
-            var habilidades =  resultado.data.abilities.map(habi => `<p>${habi.ability.name}</p>`)
+            var lista = document.querySelector('#listaHabilidades');
+            var habilidades = resultado.data.abilities.map(habi => `<p>${habi.ability.name}</p>`)
             lista.innerHTML = habilidades.join('')
             var imagem = poke_imagens[resultado.data.name]
             document.querySelector('#pokeImage').src = imagem
-            var tipos =  resultado.data.types.map(tipo => tipo.type.name)
+            var tipos = resultado.data.types.map(tipo => tipo.type.name)
             document.querySelector('#listaTipos').innerHTML = tipos.join(', ')
+            axios.get(`https://pokeapi.co/api/v2/type/${tipos[0]}`)
+                .then(resultado => {
+                    var exibe_carousel = resultado.data.pokemon.slice(0, 10)
+                    links = exibe_carousel.map(elm => {
+                        textoLink = `
+                        <a class="carousel-item" href="#"  onclick="buscarPokemon('${elm.pokemon.name}')"><img
+                            src="${poke_imagens[elm.pokemon.name]}"></a>`
+                        return textoLink
+                    })
+                    document.querySelector('#carouselPoke').innerHTML = links.join('')
+                    startCarousel();
+                })
         })
 }
 
 //Carousel
-document.addEventListener('DOMContentLoaded', function () {
+function startCarousel () {
     var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, { 'fullWidth': true, 'numVisible': 5, 'padding': 10 });
-});
+    var instances = M.Carousel.init(elems, { 'fullWidth': true, 'padding': 10 });
+};
 
