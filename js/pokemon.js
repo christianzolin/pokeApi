89d289effb3d7a0a0e9ab563227b99560a1a7d10
christianzolin <christian.koffer@gmail.com>
2020-05-19 01:15:14 -0300

    adicionando carrossel de imagens

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,4 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+//Search
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.autocomplete');
     var instances = M.Autocomplete.init(elems, options);
-  });
+});
+
+//Carousel
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {'fullWidth':true,'numVisible':5, 'padding': 10});
+});
